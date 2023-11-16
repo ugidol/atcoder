5f531f24a556be9afafea4c4dd9c6a7d5b5893cc
ugidol <hirata20070626@gmail.com>
2023-11-16 14:12:10 +0900

    abc295 D AC

--- abc295/d.cpp
+++ abc295/d.cpp
@@ -4,18 +4,15 @@ int main(void){
   string S;
   cin >> S;
   map<int,int> mp;
+  long long ans = 0;
   int k = 0;
   mp[k] += 1;
   for(int i=0;i<(int)S.size();i++){
     char c = S[i];
     int b = ( 1 << ( c - '0' ) );
     k ^= b;
+    ans += mp[k];
     mp[k] += 1;
-  }
-  long long ans = 0;
-  for(auto itr:mp){
-    int n = itr.second;
-    ans += ( n * ( n - 1 ) / 2 );
   }
   cout << ans << endl;
   return 0;
