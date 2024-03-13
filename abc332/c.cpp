beba9e78c36452131dbbd549ce1b495fa7e71a95
ugidol <hirata20070626@gmail.com>
2024-03-13 22:00:47 +0900

    abc332 c

--- abc332/c.cpp
+++ abc332/c.cpp
@@ -1,23 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
+vector<int> f(string& s, int &l, int &r){
+  vector<int> vec(3);
+  for(int i=l;i<r;i++){
+    char c = s[i];
+    vec[ c - '0' ] += 1;
+  }
+  return vec;
+}
 int main(void){
   int N,M;
   cin >> N >> M;
   string S;
   cin >> S;
   S.push_back('0');
-  map<char,int> mp;
+  //
   int ans = 0;
-  for(int i=0;i<=N;i++){
-    char c = S[i];
-    if ( c == '0' ) {
-      ans = max(ans, mp['2'] + max(0,mp['1']-M));
-      mp['1'] = 0;
-      mp['2'] = 0;
+  int left = 0;
+  int right = 0;
+  while(left<N){
+    right = left;
+    while(S[right]!='0'){
+      right += 1;
     }
-    else {
-      mp[c] += 1;
-    }
+    vector<int> v = f(S,left,right);
+    fprintf(stderr,"%d,%d\n",v[1],v[2]);
+    ans = max(ans,(v[2] + max(0,v[1] - M) ));
+    left = right + 1;
   }
   cout << ans << endl;
   return 0;
