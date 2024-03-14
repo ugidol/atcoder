2ba65489aa31bb99abde451a2e88917d6289c9b9
ugidol <hirata20070626@gmail.com>
2024-03-15 00:03:04 +0900

    abc319 c

--- abc319/c.cpp
+++ abc319/c.cpp
@@ -1,102 +1,58 @@
 #include <bits/stdc++.h>
 using namespace std;
-const vector<pair<int,int>> trg1 = {
-  {0,0},
-  {1,1},
-  {2,2},
-};
-const vector<pair<int,int>> trg2 = {
-  {0,2},
-  {1,1},
-  {2,0},
-};
-bool bingo(vector<vector<char>> &c, vector<vector<char>> &w, pair<int,int> &p){
-  int i = p.first;
-  int j = p.second;
-  w[i][j] = '*';
-  if ( w[(i+0)%3][(j+0)%3] == '*' && w[(i+0)%3][(j+1)%3] == '*' && w[(i+0)%3][(j+2)%3] == '*' ) {
-    if ( c[(i+0)%3][(j+0)%3] != c[(i+0)%3][(j+1)%3] && c[(i+0)%3][(j+1)%3] == c[(i+0)%3][(j+2)%3] ) {
-#ifdef DEBUG
-      fprintf(stderr,"{%d,%d}",i,j);
-      fprintf(stderr,"{%d,%d}",(i+0)%3,(j+1)%3);
-      fprintf(stderr,"{%d,%d}",(i+0)%3,(j+2)%3);
-      fprintf(stderr,"%c%c%c\n",c[(i+0)%3][(j+0)%3],c[(i+0)%3][(j+1)%3],c[(i+0)%3][(j+2)%3] );
-#endif
-      return true;
-    }
-  }
-  if ( w[(i+0)%3][(j+0)%3] == '*' && w[(i+1)%3][(j+0)%3] == '*' && w[(i+2)%3][(j+0)%3] == '*' ) {
-    if ( c[(i+0)%3][(j+0)%3] != c[(i+1)%3][(j+0)%3] && c[(i+1)%3][(j+0)%3] == c[(i+2)%3][(j+0)%3] ) {
-#ifdef DEBUG
-      fprintf(stderr,"{%d,%d}",i,j);
-      fprintf(stderr,"{%d,%d}",(i+1)%3,(j+0)%3);
-      fprintf(stderr,"{%d,%d}",(i+2)%3,(j+0)%3);
-      fprintf(stderr,"%c%c%c\n",c[(i+0)%3][(j+0)%3],c[(i+1)%3][(j+0)%3],c[(i+2)%3][(j+0)%3] );
-#endif
-      return true;
-    }
-  }
-  if ( find(trg1.begin(), trg1.end(), p) != trg1.end() ) {
-    if ( w[(i+0)%3][(j+0)%3] == '*' && w[(i+1)%3][(j+1)%3] == '*' && w[(i+2)%3][(j+2)%3] == '*' ) {
-      if ( c[(i+0)%3][(j+0)%3] != c[(i+1)%3][(j+1)%3] && c[(i+1)%3][(j+1)%3] == c[(i+2)%3][(j+2)%3] ) {
-#ifdef DEBUG
-        fprintf(stderr,"{%d,%d}",i,j);
-        fprintf(stderr,"{%d,%d}",(i+1)%3,(j+1)%3);
-        fprintf(stderr,"{%d,%d}",(i+2)%3,(j+2)%3);
-        fprintf(stderr,"%c%c%c\n",c[(i+0)%3][(j+0)%3],c[(i+1)%3][(j+1)%3],c[(i+2)%3][(j+2)%3] );
-#endif
-        return true;
-      }
-    }
-  }
-  if ( find(trg2.begin(), trg2.end(), p) != trg2.end() ) {
-    if ( w[(i+0)%3][(j+0)%3] == '*' && w[(i+2)%3][(j+1)%3] == '*' && w[(i+1)%3][(j+2)%3] == '*' ) {
-      if ( c[(i+0)%3][(j+0)%3] != c[(i+2)%3][(j+1)%3] && c[(i+2)%3][(j+1)%3] == c[(i+1)%3][(j+2)%3] ) {
-#ifdef DEBUG
-        fprintf(stderr,"{%d,%d}",i,j);
-        fprintf(stderr,"{%d,%d}",(i+2)%3,(j+1)%3);
-        fprintf(stderr,"{%d,%d}",(i+1)%3,(j+2)%3);
-        fprintf(stderr,"%c%c%c\n",c[(i+0)%3][(j+0)%3],c[(i+2)%3][(j+1)%3],c[(i+1)%3][(j+2)%3] );
-#endif
-        return true;
-      }
-    }
-  }
-  return false;
-}
-bool func(vector<vector<char>> c, vector<vector<char>> &w, vector<pair<int,int>> &v){
-  for(auto itr : v){
-    if ( bingo(c, w,itr) ) {
-      return false;
-    }
-  }
-  return true;
-}
 int main(void){
-  vector<vector<char>> c(3,vector<char>(3,' '));
-  for(auto &i:c){
-    for(auto &j:i){
-      cin >> j;
+  vector<vector<int>> c(3,vector<int>(3));
+  for(int i=0;i<3;i++){
+    for(int j=0;j<3;j++){
+      cin >> c[i][j];
     }
   }
-  vector<pair<int,int>> vec = {
+  vector<vector<int>> vec = {
     {0,0},{0,1},{0,2},
     {1,0},{1,1},{1,2},
     {2,0},{2,1},{2,2},
   };
-  double total = 0;
-  double ok = 0;
+  int total = 0;
+  int num = 0;
   sort(vec.begin(), vec.end());
   do {
     total += 1;
-    vector<vector<char>> w = c;
-    if ( func(c,w,vec) ) {
-      ok += 1;
+    vector<vector<int>> order(3,vector<int>(3));
+    int o=0;
+    for(auto itr:vec){
+      order[itr[0]][itr[1]] = o++;
     }
-  }
-  while(next_permutation(vec.begin(), vec.end()));
-  double ans = ok / total ;
-  cout << fixed << setprecision(18);
+    vector<vector<vector<int>>> ptn = {
+      {{0,0},{0,1},{0,2}},
+      {{1,0},{1,1},{1,2}},
+      {{2,0},{2,1},{2,2}},
+      {{0,0},{1,0},{2,0}},
+      {{0,1},{1,1},{2,1}},
+      {{0,2},{1,2},{2,2}},
+      {{0,0},{1,1},{2,2}},
+      {{2,0},{1,1},{0,2}},
+    };
+    //
+    bool disappointed = false;
+    for(auto p : ptn){
+      vector<pair<int,int>> v;
+      for(auto pp:p){
+        int i=pp[0];
+        int j=pp[1];
+        v.push_back({order[i][j],c[i][j]});
+      }
+      sort(v.begin(), v.end());
+      if ( v[0].second == v[1].second ) {
+        disappointed = true;
+      }
+    }
+    if ( ! disappointed ) {
+      num += 1;
+    }
+  }while(next_permutation(vec.begin(), vec.end()));
+  double ans = (double)num / (double)total ;
+  cout << setprecision(10);
   cout << ans << endl;
   return 0;
 }
+
