80b590671fd1f0bfb09786af8a5bfd20b30e7d7e
ugidol <hirata20070626@gmail.com>
2024-04-11 01:31:20 +0900

    abc246 d

--- abc246/d.cpp
+++ abc246/d.cpp
@@ -1,34 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
-long func(long a, long b){
-    return a * a * a + a * a * b + a * b * b + b * b * b ;
-}
-#define MX (long)(1000000L)
 int main(void){
-    long N;
-    cin >> N;
-    long ans = LONG_MAX;
-    for(long i=0;i<MX;i++){
-        long a = i;
-        long left = a;
-        long right = MX;
-        while(right>=left){
-            long mid = ( left + right ) / 2;
-            long value = func(a,mid);
-            if ( value == N ) {
-                ans = value;
-                goto out_of_loop;
-            }
-            if ( value > N ) {
-                ans = min(ans,value);
-                right = mid - 1;
-            }
-            else if ( value < N ) {
-                left = mid + 1;
-            }
-        }
+  long N;
+  cin >> N;
+  /*
+   * X = a^3 + a^2*b + a*b^2 + b^3 ;
+   */
+  function<long(long,long)> f = [&](long a, long b){
+    return a * a * a + a * a * b + a * b * b + b * b * b ;
+  };
+  long ans = 3*1e18+1;
+  /*
+   * aを固定（0～1e6)
+   */
+  for(long a=0;a<=1e6;a++){
+    /*
+     * bを二分探索(条件を満たす最小)
+     */
+    long left = -1;
+    long right = 1e6;
+    while(abs(right-left)>1){
+      long mid = ( left + right ) / 2;
+      long x = f(a,mid);
+      if ( x >= N ) {
+        right = mid;
+      }
+      else {
+        left = mid;
+      }
     }
-    out_of_loop:
-    cout << ans << endl;
-    return 0;
+    long b = right;
+    /*
+     * f(a,b)のminで更新
+     */
+    ans = min(ans,f(a,b));
+  }
+  cout << ans << endl;
+  return 0;
 }
