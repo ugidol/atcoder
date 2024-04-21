fa5e360fe3ebd2e45e0a55cf3ef1aa83c465ef96
ugidol <hirata20070626@gmail.com>
2024-04-21 14:18:59 +0900

    update

--- disjoint_set.cpp
+++ disjoint_set.cpp
@@ -1,32 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
-/* 自分用union-find */
-class UF{
+// 自分用disjoint-set
+class DSU{
 public:
-  int _sz;
-	// 要素を配列で管理する.
-	vector<int> parent;
-	explicit UF(int sz) : _sz(sz), parent(sz, -1) {
-    ;
+	int _sz; // 大きさ
+	vector<int> parent; // 要素を配列で管理する.
+	explicit DSU(int sz) : _sz(sz), parent(sz, -1) {
+		;
 	}
-	// unionは予約語なのでuniteにする.
-	void unite(int v1, int v2){
+	// v1とv2を同値類にする.
+	void merge(int v1, int v2){
 		int p1 = find(v1);
 		int p2 = find(v2);
 		// 既に同値類だったら何もしない.
 		if (p1 == p2){
-				return;
+			return;
 		}
 		// 重さ均衡(weight balancing)
-    if ( -parent[p1] < -parent[p2] ) {
-      swap(p1,p2);
-    }
-    parent[p1] += parent[p2] ;
-    parent[p2] = p1;
-    return;
+		if ( -parent[p1] < -parent[p2] ) {
+			swap(p1,p2);
+		}
+		parent[p1] += parent[p2] ;
+		parent[p2] = p1;
+		return;
 	}
-  
-	// 同値類かどうか
+	
+	// 同値類かどうか判定する
 	bool same(int v1, int v2){
 		return (find(v1) == find(v2));
 	}
@@ -47,33 +46,23 @@ public:
 		}
 		return root;
 	}
-  // mp[root] : {nodes,...};
-  map<int,vector<int>> groups(){
-    map<int,vector<int>> mp;
-    for(int i=0;i<_sz;i++){
-      int p = find(i);
-      mp[p].push_back(i);
-    }
-    /*
-    for(int i=0;i<(int)parent.size();i++){
-      if ( parent[i] < 0 ) {
-        mp[i].push_back(i);
-      }
-      else {
-        mp[ parent[i] ].push_back(i);
-      }
-    }
-    */
-    return mp;
-  }
-  void dump(){
-    for(int i=0;i<(int)parent.size();i++){
-      fprintf(stderr,"%4d|",i);
-    }
-    fprintf(stderr,"\n");
-    for(int i=0;i<(int)parent.size();i++){
-      fprintf(stderr,"%4d,",parent[i]);
-    }
-    fprintf(stderr,"\n");
-  }
+	// mp[root] : {nodes,...};
+	map<int,vector<int>> groups(){
+		map<int,vector<int>> mp;
+		for(int i=0;i<_sz;i++){
+			int p = find(i);
+			mp[p].push_back(i);
+		}
+		return mp;
+	}
+	void dump(){
+		for(int i=0;i<(int)parent.size();i++){
+			fprintf(stderr,"%4d|",i);
+		}
+		fprintf(stderr,"\n");
+		for(int i=0;i<(int)parent.size();i++){
+			fprintf(stderr,"%4d,",parent[i]);
+		}
+		fprintf(stderr,"\n");
+	}
 };
