a17e17669a66b0983dc810cee485b9421deed6ac
ugidol <hirata20070626@gmail.com>
2024-05-01 01:51:11 +0900

    abc338 b

--- abc338/b.cpp
+++ abc338/b.cpp
@@ -1,23 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(void){
-  string S;
-  cin >> S;
-  map<char,int> mp;
-  for(int i=0;i<(int)S.size();i++){
-    mp[ S[i] ] += 1;
-  }
-  int cnt = -1;
-  for(char c='a';c<='z';c++){
-    cnt = max(cnt,mp[c]);
-  }
-  char ans = ' ';
-  for(char c='a';c<='z';c++){
-    if ( mp[c] == cnt ) {
-      ans = c;
-      break;
-    }
-  }
-  cout << ans << endl;
+	string S;
+	cin >> S;
+	map<char,int> mp;
+	for(int i=0;i<(int)S.size();i++){
+		mp[S[i]] += 1;
+	}
+	int mx = -1;
+	for(auto itr:mp){
+		mx = max(mx,itr.second);
+	}
+
+	for(auto itr:mp){
+		if ( itr.second == mx ) {
+			cout << itr.first << endl;
+			break;
+		}
+	}
   return 0;
 }
