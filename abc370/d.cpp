fcab71770bae29ba2bb2a93b202c4bc2f0210e0a
ugidol <hirata20070626@gmail.com>
2024-09-14 00:19:00 +0900

    abc370 a,b,c

--- abc370/d.cpp
+++ abc370/d.cpp
@@ -1,43 +1,93 @@
 #include <bits/stdc++.h>
 using namespace std;
-// 集合stの中から、
-// "v 以上の最小の値" と "v より小さい最大の値"
-// をpairで返す
-pair<int,int> find(set<int> &st, int v){
-	pair<int,int> ret;
-	// "v 以上の最小の値"は、lower_bound()で求められる.
-	auto itr = st.lower_bound(v);
-	ret.second = *itr;
-	// "v より小さい最大の値"は、イテレータを１つ戻す
-	itr--;
-	ret.first = *itr;
-	return ret;
+class UF
+{
+public:
+	// 親かどうか(parent[p]==pなら親);
+	vector<int> parent;
+	// 子の個数
+	vector<int> children;
+	// 左端
+	vector<int> left;
+	// 右端
+	vector<int> right;
+	UF(int sz)
+		: parent(sz), children(sz, 0), left(sz), right(sz)
+	{
+		for (int i = 0; i < sz; i++)
+		{
+			parent[i] = i;
+			left[i] = i;
+			right[i] = i;
+		}
+	}
+	int find(int n)
+	{
+		// 親探しループ
+		int root = n;
+		while (parent[root] != root)
+		{
+			root = parent[root];
+		}
+		// 経路圧縮ループ
+		int p = n;
+		while (parent[p] != p)
+		{
+			int tmp = p;
+			p = parent[p];
+			parent[tmp] = root;
+		}
+		return root;
+	}
+	bool same(int v1, int v2)
+	{
+		return (find(v1) == find(v2));
+	}
+	void unite(int v1, int v2)
+	{
+		if (same(v1, v2))
+		{
+			return;
+		}
+		v1 = find(v1);
+		v2 = find(v2);
+		if (children[v1] < children[v2])
+		{
+			parent[v1] = v2;
+			children[v2] += children[v1] + 1;
+			children[v1] = 0;
+			left[v2] = std::min(left[v1], left[v2]);
+			right[v2] = std::max(right[v1], right[v2]);
+		}
+		else
+		{
+			parent[v2] = v1;
+			children[v1] += children[v2] + 1;
+			children[v2] = 0;
+			left[v1] = std::min(left[v1], left[v2]);
+			right[v1] = std::max(right[v1], right[v2]);
+		}
+	}
+	int min(int v)
+	{
+		v = find(v);
+		return left[v];
+	}
+	int max(int v)
+	{
+		v = find(v);
+		return right[v];
+	}
 };
-int main(void){
+int main(void)
+{
 	// 入力
 	int H,W;
 	cin >> H >> W ;
-	// 処理済みかどうかを行毎にsetで管理する
-	vector<set<int>> rows(H);
-	// 処理済みかどうかを列毎にsetで管理する
-	vector<set<int>> cols(W);
-
-	// 各setに番兵を入れる
-	for(int i=0;i<H;i++){
-		rows[i].insert(-1);
-		rows[i].insert(W);
-	}
-	for(int i=0;i<W;i++){
-		cols[i].insert(-1);
-		cols[i].insert(H);
-	}
-
-	for(int i=0;i<H;i++){
-		for(int j=0;j<W;j++){
-			rows[i].insert(j);
-			cols[j].insert(i);
-		}
-	}
+	// 処理済みかどうかを行毎にDSUで管理する
+	vector<UF> rows(H,UF(W));
+	// 処理済みかどうかを列毎にDSUで管理する
+	vector<UF> cols(W,UF(H));
 
 	// HxWのグリッド
 	// 'o' : 壁
@@ -45,12 +95,30 @@ int main(void){
 	vector<vector<char>> g(H,vector<char>(W,'o'));
 
 	// (h,w)のグリッドを処理済みにするための関数
-	function<void(int,int)> erase = [&](int h,int w){
-		rows[h].erase(w);
-		cols[w].erase(h);
+	function<void(int, int)> erase = [&](int h, int w)
+	{
 		g[h][w] = 'x';
+		// 上方向に隣接するマスが破壊済みの場合、併合する
+		if (h - 1 >= 0 && g[h - 1][w] == 'x')
+		{
+			cols[w].unite(h, h - 1);
+		}
+		// 下方向に隣接するマスが破壊済みの場合、併合する
+		if (h + 1 < H && g[h + 1][w] == 'x')
+		{
+			cols[w].unite(h, h + 1);
+		}
+		// 左方向に隣接するマスが破壊済みの場合、併合する
+		if (w - 1 >= 0 && g[h][w - 1] == 'x')
+		{
+			rows[h].unite(w, w - 1);
+		}
+		// 右方向に隣接するマスが破壊済みの場合、併合する
+		if (w + 1 < W && g[h][w + 1] == 'x')
+		{
+			rows[h].unite(w, w + 1);
+		}
 	};
-
 	int Q;
 	cin >> Q;
 	// クエリの数ぶんループ
@@ -67,28 +135,49 @@ int main(void){
 		}
 
 		// 壁が存在しない場合
-		// R行を左右に見て最初に現れる壁を探す
-		pair<int,int> pr = find(rows[R],C);
-		// C列を上下に見て最初に現れる壁を探す
-		pair<int,int> pc = find(cols[C],R);
-
-		// 左方向の壁を破壊する
-		if ( pr.first > -1 ) {
-			erase(R,pr.first);
+		{
+			UF& row = rows[R];
+			// rows[R]におけるCの親を探す
+			int root = row.find(C);
+			// 左方向の壁を破壊する
+			int left = row.min(root);
+			left -= 1;
+			if ( left >= 0 ) {
+				erase(R,left);
+			}
+			// 右方向の壁を破壊する
+			int right = row.max(root);
+			right += 1;
+			if ( right < W ) {
+				erase(R,right);
+			}
 		}
-		// 右方向の壁を破壊する
-		if ( pr.second < W ) {
-			erase(R,pr.second);
-		}
-		// 上方向の壁を破壊する
-		if ( pc.first > -1 ) {
-			erase(pc.first,C);
-		}
-		// 下方向の壁を破壊する
-		if ( pc.second < H ) {
-			erase(pc.second,C);
+		{
+			UF& col = cols[C];
+			// cols[C]におけるRの親を探す
+			int root = col.find(R);
+			// 上方向の壁を破壊する
+			int top = col.min(root);
+			top -= 1;
+			if ( top >= 0 ) {
+				erase(top,C);
+			}
+			// 下方向の壁を破壊する
+			int bottom = cols[C].max(root);
+			bottom += 1;
+			if ( bottom < H ) {
+				erase(bottom,C);
+			}
 		}
 	}
+#ifdef DEBUG
+	for(int i=0;i<H;i++){
+		for(int j=0;j<W;j++){
+			fprintf(stderr,"%c",g[i][j]);
+		}
+		fprintf(stderr,"\n");
+	}
+#endif
 
 	// グリッドに残った壁の数を数える
 	int ans = 0;
