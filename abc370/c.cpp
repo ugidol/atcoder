1a41cbb5edeaae2603094cb21e508c5cd371dc91
ugidol <hirata20070626@gmail.com>
2024-09-14 11:40:00 +0900

    abc370 c

--- abc370/c.cpp
+++ abc370/c.cpp
@@ -1,32 +1,62 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <set>
+#include <string>
+#include <algorithm>
+#include <functional>
 using namespace std;
+// 文字列SとTの長さは等しい制約
 int main(void){
+	// 入力
 	string S,T;
 	cin >> S >> T;
-	function<void(vector<string>&,string)> f = [&](vector<string> &stk,string s){
-		if ( s == T ) {
-			return ;
-		}
-		set<string> st;
-		for(int i=0;i<(int)T.size();i++){
-			if ( s[i] == T[i] ) {
-				continue;
+
+	vector<string> ans;
+	// 辞書順最小の文字列をset<string>で管理する版
+	function<void(void)> solve1 = [&](){
+		string s = S;
+		while(s!=T){
+			set<string> st;
+			for(int i=0;i<(int)T.size();i++){
+				if ( s[i] == T[i] ) {
+					continue;
+				}
+				string ss(s);
+				ss[i] = T[i];
+				st.insert(ss);
 			}
-			string ss(s);
-			ss[i] = T[i];
-			st.insert(ss);
-#ifdef DEBUG
-			fprintf(stderr,"[%s]\n",ss.c_str());
-#endif
+
+			s = *st.begin();
+			ans.push_back(s);
 		}
-		string ss = *st.begin();
-		stk.push_back(ss);
-		f(stk,ss);
 	};
-	vector<string> stk;
-	f(stk,S);
-	cout << stk.size() << endl;
-	for(auto itr:stk){
+	// 辞書順最小の文字列をminで比較する版
+	function<void(void)> solve2 = [&](){
+		// 初期値はS
+		string s = S;
+		// Tになるまで繰り返し
+		while(s!=T){
+			// sを１文字ずつ交換していって辞書順最小の文字列を探す
+			string min(T.size(),'z');
+			for(int i=0;i<(int)T.size();i++){
+				if ( s[i] == T[i] ) {
+					continue;
+				}
+				string ss(s);
+				ss[i] = T[i];
+				min = std::min(min,ss);
+			}
+			s = min;
+
+			ans.push_back(s);
+		}
+	};
+
+	solve2();
+	
+	// 出力
+	cout << ans.size() << endl;
+	for(auto itr:ans){
 		cout << itr << endl;
 	}
   return 0;
