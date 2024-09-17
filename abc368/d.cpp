a9a15a7bb38a5b608c55b8c55d97154a6237b7e5
ugidol <hirata20070626@gmail.com>
2024-09-18 03:01:09 +0900

    abc368 d

--- abc368/d.cpp
+++ abc368/d.cpp
@@ -6,7 +6,9 @@ using namespace std;
 int main(void){
 	int N,K;
 	cin >> N >> K;
+	// グラフ g を隣接リストとして初期化
 	vector<vector<int>> g(N,vector<int>(0));
+	// N-1 本の辺の情報を入力し、グラフを構築
 	for(int i=0;i<N-1;i++){
 		int A,B;
 		cin >> A >> B;
@@ -15,6 +17,7 @@ int main(void){
 		g[A].push_back(B);
 		g[B].push_back(A);
 	}
+	// K個のノードを管理
 	vector<bool> V(N);
 	for(int i=0;i<K;i++){
 		int v;
@@ -22,6 +25,33 @@ int main(void){
 		v -= 1;
 		V[v] = true;
 	}
+
+	// 探索済みノードを管理するフラグ配列
+	vector<bool> visited(N,false);
+
+	// 深さ優先探索(DFS)
+	// ノードnの子要素の有効なノード数をカウントして返却
+	function<int(int)> dfs = [&](int n){
+		int cnt = 0;
+		// ノードnを探索済みにする
+		visited[n] = true;
+
+		// 未探索の子ノードに対して再帰的にDFS探索
+		vector<int>& nodes = g[n];
+		for(auto nn:nodes){
+			if ( visited[nn] ) {
+				continue;
+			}
+			cnt += dfs(nn);
+		}
+		// 子ノードに有効なノードがあれば、自分自身もカウント
+		if ( cnt > 0 || V[n] ) {
+			cnt += 1;
+		}
+		return cnt;
+	};
+	
+	// 最初に見つけた有効なノードをrootとする
 	int root = -1;
 	for(int i=0;i<N;i++){
 		if ( V[i] ) {
@@ -30,22 +60,7 @@ int main(void){
 		}
 	}
 
-	vector<bool> visited(N,false);
-	function<int(int)> dfs = [&](int n){
-		visited[n] = true;
-		vector<int>& nodes = g[n];
-		int cnt = 0;
-		for(auto nn:nodes){
-			if ( visited[nn] ) {
-				continue;
-			}
-			cnt += dfs(nn);
-		}
-		if ( cnt > 0 || V[n] ) {
-			cnt += 1;
-		}
-		return cnt;
-	};
+	// DFS実行
 	int ans = dfs(root);
 	cout << ans << endl;
 
