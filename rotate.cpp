c0640cff30617d8e0b2a563b1f5589635316e4ca
ugidol <hirata20070626@gmail.com>
2024-10-13 16:48:13 +0900

    abc375

--- rotate.cpp
+++ rotate.cpp
@@ -8,13 +8,22 @@ void rotate(vector<vector<char>> &vec){
     for(int j=0;j<W;j++){
 			
 			// 二次元配列を右に90度回転させる
-      //vec[i][j] = src[H-1-j][i];
+      // 転送先主体
+      //vec[i][j]  = src[H-1-j][i];
+      // 転送元主体
+      //vec[j][H-1-i] = src[i][j];
 			
 			// 二次元配列を180度回転させる
+      // 転送先主体
 			//vec[i][j] = src[H-1-i][W-1-j];
+      // 転送元主体
+      //vec[H-1-i][W-1-j] src A[i][j];
 			
-			// 二次元配列を左に90度回転させる
+			// 二次元配列を270度回転させる
+      // 転送先主体
 			//vec[i][j] = src[j][H-1-i];
+      // 転送元主体
+      //vec[H-1-j][i] = src[i][j];
 			
     }
   }
