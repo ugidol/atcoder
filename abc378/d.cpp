b9dfb0002dabf85ceaa7ab7317ebd084ec2db0f6
ugidol <hirata20070626@gmail.com>
2024-11-03 00:39:11 +0900

    abc378 d

--- abc378/d.cpp
+++ abc378/d.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(void){
+	// 入力読取
 	int H,W,K;
 	cin >> H >> W >> K;
 	vector<string> g(H);
@@ -9,38 +10,55 @@ int main(void){
 		cin >> s;
 		g[i] = s;
 	}
-	vector<array<int,2>> dir = { {1,0},{0,1},{-1,0},{0,-1}};
+	// 4方向への移動を示す定数
+	vector<array<int,2>> dir = { 
+		{-1, 0}, // UP
+		{ 1, 0}, // DOWN
+		{ 0,-1}, // LEFT
+		{ 0, 1}, // RIGHT
+	};
 	long ans = 0;
+	// H * W の各グリッドについてdfs
 	for(int i=0;i<H;i++){
 		for(int j=0;j<W;j++){
+			// 壁は探索不要
 			if ( g[i][j] == '#' ) {
 				continue;
 			}
-			set<pair<int,int>> st;
-			//
-			function<void(int,int,int)> dfs = [&](int x,int y, int k){
-				pair<int,int> p = make_pair(x,y);
-				if ( k == K ) {
+			// --------------------------------------------------------------------------
+			// DFS(深さ優先探索)
+			function<void(int,int,int,set<pair<int,int>>&)> dfs = [&](int x,int y, int depth, set<pair<int,int>> &visited){
+				// 移動回数がKになったら探索終了
+				if ( depth == K ) {
 					ans += 1;
 					return;
 				}
-				st.insert(p);
+				// {x,y}を探索済みにする
+				visited.insert({x,y});
+				// ４方向についてdfs
 				for(auto d:dir){
 					int xx = x + d[0];
 					int yy = y + d[1];
+					// 範囲外と壁は探索不要
 					if ( xx < 0 || xx >= H || yy < 0 || yy >= W || g[xx][yy] == '#' ) {
 						continue;
 					}
-					pair<int,int> pp = make_pair(xx,yy);
-					if ( st.find(pp) == st.end() ) {
-						dfs(xx,yy,k+1);
+					// 探索済みは探索しない
+					if ( visited.find({xx,yy}) != visited.end() ) {
+						continue;
 					}
+					// dfs再帰
+					dfs(xx,yy,depth+1,visited);
 				}
-				st.erase(p);
+				// 未探索状態に戻す
+				visited.erase({x,y});
 			};
-			//
+			// --------------------------------------------------------------------------
+			
+			// グリッド[i][j]についてDFS
+			set<pair<int,int>> st;
 			int k=0;
-			dfs(i,j,k);
+			dfs(i,j,k,st);
 		}
 	}
 	cout << ans << endl;
